04da67c6678207a3124a8a3fcf90972e82670716
aliozsoy1 <aliozsoy237@gmail.com>
2024-07-19 01:51:55 +0300

    ..

--- app/page.js
+++ app/page.js
@@ -7,7 +7,7 @@ export default function Home() {
     <div className="flex">
       <Sidebar />
       <div className="flex-1 p-10">
-        <h1 className="text-2xl">Hi, Ali Özsoy</h1>
+        <h1 className="text-2xl">Hi, Ali Özsoyy</h1>
       </div>
     </div>
   );
